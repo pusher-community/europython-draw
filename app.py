c67488a399331e16d5f38b6ee2f064072006b0ea
Ben Foxall <ben@pusher.com>
2016-07-19 01:19:21 +0200

    Add static landing page

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def root():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run()
